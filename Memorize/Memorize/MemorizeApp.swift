f2ed133605e50d2d04db3f641f3f9373cd12204d
Jang Hun Choi <jang@jangs-mbp-2.mynetworksettings.com>
2023-10-28 17:01:04 -0400

    default setup

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jang Hun Choi on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
